2adbab58ccc19bccf7b39814242977c992f167e0
Richard_Sun_1688 <richards@emonster.ca>
2023-07-22 14:56:28 -0700

    first commit

---
+++ src/test/java/com/nemogz/CounterApp/CounterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemogz.CounterApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
